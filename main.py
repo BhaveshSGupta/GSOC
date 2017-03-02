2a54605074c67c41db7148c2a78e9419d55c2f16
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-03-02 14:08:51 +0530

    Data Gathering

---
+++ main.py
@@ -0,0 +1,7 @@
+# #import urllib2
+# from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+# url  = "https://summerofcode.withgoogle.com/organizations/"
+# page = urlopen(url)
+# soup = BeautifulSoup(page,"html5lib")
+# print (soup.title)
---
+++ readME.md
@@ -0,0 +1,5 @@
+#GSOC 17
+
+Data set fetched from 'https://summerofcode.withgoogle.com/api/program/current/organization/'
+
+For easier access and checks
